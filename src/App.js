e34eb21b88b5288e0d23b5f52d7c7fc2ad8ef3ae
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-10-02 18:50:33 +0900

    buttonLink

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
     </Father>
   );
 }
